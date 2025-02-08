547090f95abad9e449f8b6868da5c0073ff29eeb
Kundank7 <149052665+Kundank7@users.noreply.github.com>
2025-02-08 10:11:41 +0530

    Update HDHub4u_Movie_Scraper.py

--- HDHub4u_Movie_Scraper.py
+++ HDHub4u_Movie_Scraper.py
@@ -34,8 +34,6 @@ def get_movie_details():
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
-import os
-
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))  # Uses PORT from the environment
     app.run(host='0.0.0.0', port=port)
